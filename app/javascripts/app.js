7ef47da334591e29c2b1c5722f71df2b5cdff069
David Cristofaro <david@dtcristo.com>
2017-05-07 23:10:54 +1000

    Add loading animation on submission

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,12 +1,10 @@
-var meta;
 $(function(){
   var metaAbi = [{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getBalanceInEth","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"receiver","type":"address"},{"name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"name":"sufficient","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"}];
   var metaAddress = "0x58c2360e5d3258a1c85038f4258821d3c8d083c1";
-  meta = web3.eth.contract(metaAbi).at(metaAddress);
+  var meta = web3.eth.contract(metaAbi).at(metaAddress);
 
   let accounts = web3.eth.accounts,
       account;
-
 
   var event = meta.Transfer();
   event.watch(function(error, result){
@@ -30,25 +28,24 @@ $(function(){
 
   function onMetaBalanceChange() {
     var bal = getMetaBalance(account);
-    $(".account-balances__meta-coin").text(bal);
+    $(".account-balances__meta-coin").text(bal).show().prev(".loading").hide();
   }
 
   function onEtherBalanceChange() {
     var bal = getEtherBalance(account);
-    $(".account-balances__ether").text(bal);
+    $(".account-balances__ether").text(bal).show().prev(".loading").hide();
   }
-
 
   function onAccountChange() {
     account = $("#account-select").val();
+    $(".account-balances__value .loading").show().next().hide();
     onMetaBalanceChange();
     onEtherBalanceChange();
   }
 
   onAccountChange();
 
-  $("#account-switcher").on('change', onAccountChange);
-
+  $("#account-select").on('change', onAccountChange);
 
   function getMetaBalance(acc) {
     return meta.getBalance.call(acc).toNumber();
@@ -61,7 +58,21 @@ $(function(){
   function sendCoin() {
     var amount = $("#send-meta-amount").val();
     var destination = $("#send-meta-address").val();
-    meta.sendCoin(destination, amount, {from: account})
+
+    meta.sendCoin.sendTransaction(destination, amount, {from: account}, function(error, result){
+      if(!error){
+        alert("Success");
+      } else {
+        alert("Something went wrong :(");
+      }
+      $(".send-meta__button").removeAttr("disabled").children().toggle();
+      $(".send-meta")[0].reset();
+    });
+
+    $(".send-meta__button").attr("disabled", "disabled");
+    $(".send-meta__button").children().toggle();
+    $(".account-balances__value .loading").show().next().hide();
+    return false;
   }
 
   $(".send-meta__button").click(sendCoin);
