42f633d5e30e57be27518da70f480d4fcf6f531a
David Cristofaro <david@dtcristo.com>
2017-05-07 15:54:22 +1000

    Add code

--- truffle.js
+++ truffle.js
@@ -1,4 +1,16 @@
+var DefaultBuilder = require("truffle-default-builder");
+
 module.exports = {
+  build: new DefaultBuilder({
+    "index.html": "index.html",
+    "app.js": [
+      "javascripts/app.js"
+    ],
+    "app.css": [
+      "stylesheets/app.css"
+    ],
+    "images/": "images/"
+  }),
   networks: {
     development: {
       host: "localhost",
