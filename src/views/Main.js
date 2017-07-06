74495e6655c919d4055ae4624c8f8264e340adb6
BrendanTimmons <brendantimmons@live.com.au>
2017-07-06 19:03:36 +1000

    - Small refactor of the components to make it a little bit more readable.

--- src/views/Main.js
+++ src/views/Main.js
@@ -13,38 +13,55 @@ window.MetaCoin = contract(metacoin_artifacts);
 let accounts;
 let account;
 
-export default class Main {
+const accountSwitcherUI = {
   view(){
-    return m(".container", [
-      m("h1", "MetaCoin"),
+    return (
       m(".content.account-switcher", [
         m("label[for='account-select']", "Account"),
         m("select.account-switcher__select[id='account-select'][name='account-switcher-select']")
-      ]),
+      ])
+    );
+  }
+};
+
+const loaderUI = {
+  view(){
+    return(
+      m("span.loading", [
+        m("span.loader-1", "."),
+        m("span.loader-2", "."),
+        m("span.loader-3", ".")
+      ])
+    );
+  }
+}
+
+const accountBalancesUI = {
+  view(){
+    return (
       m(".content.account-balances", [
         m("span.account-balances__label",
           m("strong", "MetaCoin Balance:")
         ),
         m("span.account-balances__value", [
-          m("span.loading", [
-            m("span.loader-1", "."),
-            m("span.loader-2", "."),
-            m("span.loader-3", ".")
-          ]),
+          m(loaderUI),
           m("span.account-balances__meta-coin")
         ]),
         m("span.account-balances__label",
           m("strong", "Ether Balance:")
         ),
         m("span.account-balances__value",[
-          m("span.loading", [
-            m("span.loader-1", "."),
-            m("span.loader-2", "."),
-            m("span.loader-3", ".")
-          ]),
+          m(loaderUI),
           m("span.account-balances__ether")
         ])
-      ]),
+      ])
+    );
+  }
+};
+
+const transactionUI = {
+  view(){
+    return(
       m("form.content.send-meta", [
         m("h2.send-meta__title", "Send MetaCoin"),
         m("ul.input-list", [
@@ -53,21 +70,26 @@ export default class Main {
             m("input.send-meta__input[id='send-meta-address'][name='address'][placeholder='e.g. 0x0359b919f373b18e29ff92c1687990549e51875a'][required=''][type='text'][value='']")
           ]),
           m("li.input-list__item", [
-            m("label[for='send-meta-amount']",
-              "Amount"
-            ),
+            m("label[for='send-meta-amount']", "Amount"),
             m("input.send-meta__input[id='send-meta-amount'][min='0'][name='amount'][required=''][type='number']")
           ])
         ]),
         m("button.button.send-meta__button[type='submit']", [
           m("span.default", "Send"),
-          m("span.loading", [
-            m("span.loader-1", "."),
-            m("span.loader-2", "."),
-            m("span.loader-3", ".")
-          ]),
+          m(loaderUI)
         ])
       ])
+    );
+  }
+};
+
+export default class Main {
+  view(){
+    return m(".container", [
+      m("h1", "MetaCoin"),
+      m(accountSwitcherUI),
+      m(accountBalancesUI),
+      m(transactionUI)
     ]);
   }
 
