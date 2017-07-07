76adead86356cc7b0b2c2661cb3000106746f8f6
The Dim <brendan@hhd.com.au>
2017-07-07 12:50:26 +1000

    - Swapped async fetching of accounts out.

--- src/views/Main.js
+++ src/views/Main.js
@@ -18,20 +18,7 @@ function metaModel(){
 
 const actions = {
   fetchAccounts(model){
-    web3.eth.getAccounts(function(err, accs) {
-      if (err != null) {
-        alert("There was an error fetching your accounts.");
-        return;
-      }
-
-      if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-        return;
-      }
-
-      model.accounts = accs || [];
-      console.log(model.accounts);
-    });
+    model.accounts = web3.eth.accounts;
   },
   switchAccounts(){
     // TOOD swap accounts logic here.
@@ -40,13 +27,13 @@ const actions = {
 
 const accountSwitcherUI = {
   view({ attrs }){
-    console.log(attrs);
-    // issue: this doesn't get called at the right time... so the array is empty.
     return (
       m(".content.account-switcher", [
         m("label[for='account-select']", "Account"),
         m("select.account-switcher__select[id='account-select'][name='account-switcher-select']", [
-          // TODO map over accounts and create options
+          attrs.items.map((account) =>
+            m("option", account)
+          )
         ])
       ])
     );
